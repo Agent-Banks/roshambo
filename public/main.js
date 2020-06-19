a0b6d8cdc7bc4695f5e268dedb7723f07f728485
CodyBanks <codydouglasbanks@gmail.com>
2020-06-19 18:59:43 -0400

    added a finish turn option

--- public/main.js
+++ public/main.js
@@ -4,14 +4,25 @@ let ties = 0
 let playerOneChoice = ''
 let playerTwoChoice = ''
 
-function handlePlayerOneRockChoice(event) {
+function handlePlayerOneRockChoice() {
   document.querySelector('.playerOneCard .playerOneChoiceImg').src =
     'https://www.rpsgame.org/assets/img/rock.svg'
+  playerOneChoice = 'rock'
+}
+
+function handlePlayerOneEndTurn() {
+  const playerOneCard = document.querySelector(' .playerOneCard')
+  playerOneCard.style.display = 'none'
 }
 
 const main = () => {
   const playerOneRock = document.querySelector(' .playerOneCard .rock ')
   playerOneRock.addEventListener('click', handlePlayerOneRockChoice)
+
+  const endPlayerOneTurn = document.querySelector(
+    ' .playerOneCard .endPlayerOneTurn '
+  )
+  endPlayerOneTurn.addEventListener('click', handlePlayerOneEndTurn)
 }
 
 document.addEventListener('DOMContentLoaded', main)
